2af460daeb65f77e6ca2026f6caf564c34c80d93
Vestigialearth <38117731+Vestigialearth@users.noreply.github.com>
2019-01-29 17:40:57 -0600

    Update PatternTest.java

--- PatternTest.java
+++ PatternTest.java
@@ -5,34 +5,43 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ *Tests to see that the class "promptString" doesnt return with a null identifier
+ *
  */
-public class AppTest 
-    extends TestCase
+public class PatternTest
+    extends Pattern
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+        public PatternUtilTest( String PatternUtilTest )
     {
-        super( testName );
+       assertNotNull(["This is not null",] DIM)
+       assertNotNull(["This is not null",] DEVICE)
+       assertNotNull(["This is not null",] GROUP)
+       assertNotNull(["This is null",] ADD_DEVICE)
+       assertNotNull(["This is not null",] REMOVE_DEVICE)
+       assertNotNull(["This is not null",] REMOVE_GROUP)
+       assertNotNull(["This is not null",] DIM_PATTERN)
+       assertNotNull(["This is null",] DEVICE_PATTERN)
+       assertNotNull(["This is not null",] GROUP_PATTERN)
+       assertNotNull(["This is not null",] ADD_DEVICE_PATTERN)
+       assertNotNull(["This is not null",] REMOVE_DEVICE_PATTERN)
+       assertNotNull(["This is null",] REMOVE_GROUP_PATTERN)
+        super(PatternUtilTest);
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
+   
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AbstractTest.class );
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigourous Test 
      */
-    public void testApp()
+    public void AbstractTest()
     {
-        assertTrue( true );
-    }
-}
+        assertTrue(true);
+    }   A

eeb72dc3ed16634c6df7f38d471d90cf28f04bf4
Vestigialearth <38117731+Vestigialearth@users.noreply.github.com>
2019-01-29 12:28:09 -0600

    Update AbstractTest.java

--- AbstractTest.java
+++ AbstractTest.java
@@ -4,28 +4,65 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AbstractTest 
+    extends Abstract
 {
+    
     /**
      * Create the test case
      *
      * @param testName name of the test case
-     */
-    public AppTest( String testName )
+     */@Test
+     
+    public ReaderShouldNotReturnNull( )
     {
-        super( testName );
+        assertNotNull(["This is not null"] reader)
+        super( AbstractManipulator );
     }
 
+        /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */@Test
+    public ToggleNameShouldNotReturnNull()
+    {
+        assertNotNull(["This is not null"] toggleName)
+        super(ToggleNameShouldNotReturnNull);
+    }
+    
+        /**
+     * Create the test case
+     *
+     * @param  name of the test case
+     */@Test
+    public InputShouldNotReturnNull()
+    {
+        assertNotNull(["This is not null"] input)
+        super(InputShouldNotReturnNull);
+    }
+    
+            /**
+     * Create the test case
+     *
+     * @param  name of the test case
+     */@Test
+    public CommandShouldNotReturnNull()
+    {
+        assertNotNull(["This is not null"] command)
+        super(CommandShouldNotReturnNull);
+    }
+    
     /**
      * @return the suite of tests being tested
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AbstractTest.class );
     }
 
     /**

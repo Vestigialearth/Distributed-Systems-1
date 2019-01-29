eb01c683059d3856a18f8c6529830406a3e313ca
Vestigialearth <38117731+Vestigialearth@users.noreply.github.com>
2019-01-29 16:55:48 -0600

    Update EventTest.java

--- EventTest.java
+++ EventTest.java
@@ -5,35 +5,75 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ *Tests to see that the class "promptString" doesnt return with a null identifier
+ *
  */
-public class AppTest 
-    extends TestCase
+public class EventHandlerManipulatorTest
+    extends EventHandlerManipulator
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+     
+    public EventHandlerManipulatorrTest( String EventHandlerManipulatorrTest )
     {
-        super( testName );
+       assertNotNull(["This is not null",] taskManager)
+       assertNotNull(["This is not null",] deviceManager)
+       assertNotNull(["This is not null",] eventBroker)
+       assertNotNull(["This is not null",] scheduler)
+        super(EventHandlerManipulatorrTest);
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
+    
+        public removeEventListenerTest( String removeEventListenerTest )
+    {
+       assertNotNull(["This is not null",] id)
+        super(removeEventListenerTest);
+    }
+    
+    public DateTest( String DateTest )
+    
+    {
+       assertNotNull(["This is not null",] date)
+       assertNotNull(["This is not null",] units)
+       assertNotNull(["This is not null",] numberOfUnits)
+        super(DateTest);
+    }
+    
+        public addEventListenerTest( String addEventListenerTest )
+    {
+       assertNotNull(["This is not null",] name)
+       assertNotNull(["This is not null",] nameRegex)
+       assertNotNull(["This is not null",] task)
+       assertNull(["This is null",] listenOnTask)
+        super(addEventListenerTest);
+    }
+    
+        public ScheduledTaskTypeTest( String ScheduledTaskTypeTest )
+    {
+       assertNotNull(["This is not null",] toggle)
+       assertNotNull(["This is not null",] turnOn)
+       assertNotNull(["This is not null",] turnOff)
+       assertNull(["This is null",] delayInMinutes)
+        super(ScheduledTaskTypeTest);
+    }
+   
+        public createNameConditionTest( String createNameConditionTest )
+    {
+       assertNotNull(["This is not null",] namePattern)
+        super(createNameConditionTest);
+    }
+    
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AbstractTest.class );
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigourous Test 
      */
-    public void testApp()
+    public void AbstractTest()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
-}
-

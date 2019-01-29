538daa4f661c8a558b77dd8de58fde5a0476edd9
Vestigialearth <38117731+Vestigialearth@users.noreply.github.com>
2019-01-29 17:27:46 -0600

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -5,34 +5,49 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ *Tests to see that the class "promptString" doesnt return with a null identifier
+ *
  */
-public class AppTest 
-    extends TestCase
+public class MainTest
+    extends Main
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+     
+    public MainMenuTest( String MainMenuTest )
     {
-        super( testName );
+       assertNotNull(["This is not null",] reader)
+       assertNotNull(["This is not null",] str)
+       assertNotNull(["This is not null",] device)
+       assertNotNull(["This is not null",] id)
+        super(EventHandlerManipulatorrTest);
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
+    
+      public removeEventListenerTest( String removeEventListenerTest )
+    {
+       assertNotNull(["This is not null",] id)
+        super(removeEventListenerTest);
+    }
+    
+      public TogglableGroupTest( String TogglableGroupTest )
+    
+    {
+       assertNotNull(["This is not null",] toggleName)
+        super(TogglableGroupTest);
+    }
+    
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AbstractTest.class );
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigourous Test 
      */
-    public void testApp()
+    public void AbstractTest()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
-}
